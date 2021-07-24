1aa0565f724c5297121adb908cb3bc39c0da9042
Nawal C <nawal.abdul-careem@capgemini.com>
2021-07-24 20:37:00 +0100

    added resoure group and virtual network

--- main.tf
+++ main.tf
@@ -1,15 +1,18 @@
-# Configure the Azure provider
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 2.65"
-    }
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "UK South"
+
+  tags = {
+    Environment = "Terraform Getting Started"
+    Team        = "DevOps"
   }
 
-  required_version = ">= 0.14.9"
 }
 
-provider "azurerm" {
-  features {}
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "UK South"
+  resource_group_name = azurerm_resource_group.rg.name
 }
